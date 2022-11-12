8f6d7a0bd1faf03e09f65e0c5d2e4ef0a6b33349
Minseop <alstjqaos@gmail.com>
2022-11-12 15:58:20 +0900

    Initial Commit

---
+++ NotificationSample/NotificationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationSampleApp.swift
+//  NotificationSample
+//
+//  Created by 김민섭 on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct NotificationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
